a2af9b1b3dd0f6effa145982bf4d744c2406911a
Amanda Jin Liu <ajliu72@gmail.com>
2025-08-19 20:09:54 -0700

    prettier and remove comment

--- jwvideo.js
+++ jwvideo.js
@@ -8,10 +8,9 @@ var GRAB_THRESH = 0.3;
 var VOL_RATIO = 0.75;
 var SEEK_RATIO = 1.0;
 
-var ASPECT_RATIO = 9/16;
-var INIT_WIDTH = window.innerWidth/2;
+var ASPECT_RATIO = 9 / 16;
+var INIT_WIDTH = window.innerWidth / 2;
 // var INIT_HEIGHT = 450;
-
 
 // CHANGE_THRESH: min num frames btwn commands
 var FULL_CHANGE_THRESH = 90;
@@ -28,25 +27,23 @@ var X_CHANGE = 20;
 var CLOSE_ENOUGH_Z = 60;
 var X_AXIS_SHIFT = 100;
 
-
 function timepad(s) {
-  var res = s.split(':');
+  var res = s.split(":");
   if (res[1].length < 2) {
-    return s + '0';
+    return s + "0";
   }
   return s;
-
 }
 
 function round5(x) {
-  return Math.ceil(x/5)*5;
+  return Math.ceil(x / 5) * 5;
 }
 
 function vectorsCloseEnough(v1, v2, thresh) {
   if (v1.length != v2.length) {
     return false;
   }
-  for (var i=0; i < v1.length; i++ ) {
+  for (var i = 0; i < v1.length; i++) {
     if (Math.abs(v1[i] - v2[i]) > thresh) {
       return false;
     }
@@ -66,7 +63,7 @@ function resetState(gestureStart, state) {
   gestureStart.init_right = null;
   if (newState) {
     // console.log(gestureStart);
-    if (gestureStart.name == 'full_single') {
+    if (gestureStart.name == "full_single") {
       console.log(gestureStart);
     }
   }
@@ -77,20 +74,18 @@ function vectorToString(v) {
 }
 
 window.onload = () => {
-
   // toggle instructions visibility
-  document.getElementById('instButton').addEventListener('click', () => {
-    var inst = document.getElementById('instImg');
-    if (inst.style.display == 'none') {
-      inst.style.display = 'block';
+  document.getElementById("instButton").addEventListener("click", () => {
+    var inst = document.getElementById("instImg");
+    if (inst.style.display == "none") {
+      inst.style.display = "block";
     } else {
-      inst.style.display = 'none';
+      inst.style.display = "none";
     }
   });
 
-
-  const player = jwplayer('player');
-  document.getElementById('feedback').style.width = INIT_WIDTH - 10;
+  const player = jwplayer("player");
+  document.getElementById("feedback").style.width = INIT_WIDTH - 10;
 
   function handleFileSelect(evt) {
     var files = evt.target.files;
@@ -98,29 +93,66 @@ window.onload = () => {
     // set up video player
 
     player.setup({
-      "file": "videos/"+files[0].name,
-      "height": ASPECT_RATIO*INIT_WIDTH,
-      "width": INIT_WIDTH,
-      "controls":true,
-      "controls.position": "bottom"
+      file: "videos/" + files[0].name,
+      height: ASPECT_RATIO * INIT_WIDTH,
+      width: INIT_WIDTH,
+      controls: true,
+      "controls.position": "bottom",
     });
 
-    document.getElementById('feedback').style.display = 'block';
-
+    document.getElementById("feedback").style.display = "block";
   }
 
-  document.getElementById('video_input').addEventListener('change', handleFileSelect, false);
+  document
+    .getElementById("video_input")
+    .addEventListener("change", handleFileSelect, false);
 
   // Leap logic
-  var controllerOptions = {enableGestures: true};
-  var togglePlayGestureStart = {'name': 'playPause', 'state': 'READY', 'frames': 0, 'frames_thresh': PLAY_CHANGE_THRESH, 'init_pos': null};
-  var toggleFullGestureStart = {'name': 'full', 'state': 'READY', 'frames': 0, 'frames_thresh': FULL_CHANGE_THRESH, 'init_left': null, 'init_right': null}; // [state, frames, thresh], where state = READY, STARTED, WAITING
-  var volumeGestureStart = {'name': 'volume', 'state':'READY', 'init_pos': null, 'init_vol': 0, 'frames_thresh': VOL_CHANGE_THRESH};
-  var seekGestureStart = {'name': 'seek', 'state': 'READY', 'init_pos': null, 'init_time': 0, 'frames_thresh': SEEK_CHANGE_THRESH};
+  var controllerOptions = { enableGestures: true };
+  var togglePlayGestureStart = {
+    name: "playPause",
+    state: "READY",
+    frames: 0,
+    frames_thresh: PLAY_CHANGE_THRESH,
+    init_pos: null,
+  };
+  var toggleFullGestureStart = {
+    name: "full",
+    state: "READY",
+    frames: 0,
+    frames_thresh: FULL_CHANGE_THRESH,
+    init_left: null,
+    init_right: null,
+  }; // [state, frames, thresh], where state = READY, STARTED, WAITING
+  var volumeGestureStart = {
+    name: "volume",
+    state: "READY",
+    init_pos: null,
+    init_vol: 0,
+    frames_thresh: VOL_CHANGE_THRESH,
+  };
+  var seekGestureStart = {
+    name: "seek",
+    state: "READY",
+    init_pos: null,
+    init_time: 0,
+    frames_thresh: SEEK_CHANGE_THRESH,
+  };
 
-  var toggleFullSingleGestureStart = {'name': 'full_single', 'state': 'READY', 'startFist': true, 'frames': 0, 'frames_thresh': FULL_SINGLE_THRESH}
+  var toggleFullSingleGestureStart = {
+    name: "full_single",
+    state: "READY",
+    startFist: true,
+    frames: 0,
+    frames_thresh: FULL_SINGLE_THRESH,
+  };
 
-  var customGestures = [togglePlayGestureStart, toggleFullGestureStart, volumeGestureStart, seekGestureStart];
+  var customGestures = [
+    togglePlayGestureStart,
+    toggleFullGestureStart,
+    volumeGestureStart,
+    seekGestureStart,
+  ];
 
   var leftHand;
   var rightHand;
@@ -128,40 +160,46 @@ window.onload = () => {
   var hands;
   var feedbackString;
 
-  player.on('ready', () => {
+  player.on("ready", () => {
     player.setVolume(50);
   });
 
-  Leap.loop(controllerOptions, function(frame) {
-
-    if (player.getState() == 'idle') {
-      feedbackString = 'ready';
+  Leap.loop(controllerOptions, function (frame) {
+    if (player.getState() == "idle") {
+      feedbackString = "ready";
     } else {
       feedbackString = player.getState();
     }
     // Body of callback function
 
-
     // sloppy loop through all gestures
     // if enough time has passed between switching screen size, now ready to switch again.
     if (frame.hands.length === 0) {
       // feedbackString = "ready";
-      resetState(togglePlayGestureStart, 'READY');
-      resetState(toggleFullGestureStart, 'READY');
-      resetState(volumeGestureStart, 'READY');
-      resetState(seekGestureStart, 'READY');
+      resetState(togglePlayGestureStart, "READY");
+      resetState(toggleFullGestureStart, "READY");
+      resetState(volumeGestureStart, "READY");
+      resetState(seekGestureStart, "READY");
       // resetState(toggleFullSingleGestureStart, 'READY');
     }
-    customGestures = [togglePlayGestureStart, toggleFullGestureStart, volumeGestureStart, seekGestureStart];
+    customGestures = [
+      togglePlayGestureStart,
+      toggleFullGestureStart,
+      volumeGestureStart,
+      seekGestureStart,
+    ];
 
-    for (var f=0; f < customGestures.length; f++) {
+    for (var f = 0; f < customGestures.length; f++) {
       var gestureStart = customGestures[f];
-      gestureStart.frames += 1 // increment frames
-      if (gestureStart.frames >= gestureStart.frames_thresh && gestureStart.state == 'WAITING') {
+      gestureStart.frames += 1; // increment frames
+      if (
+        gestureStart.frames >= gestureStart.frames_thresh &&
+        gestureStart.state == "WAITING"
+      ) {
         // ready to change state
         // customGestures[f][0] = 'READY';
         // customGestures[f][1] = 0;
-        resetState(customGestures[f], 'READY');
+        resetState(customGestures[f], "READY");
       }
     }
 
@@ -172,24 +210,26 @@ window.onload = () => {
     // toggleFullSingleGestureStart = customGestures[4];
 
     // detect pause gesture
-    if (frame.hands.length == 1 && frame.hands[0].palmPosition[2] <= CLOSE_ENOUGH_Z) {
-
-      resetState(toggleFullGestureStart, 'READY');
+    if (
+      frame.hands.length == 1 &&
+      frame.hands[0].palmPosition[2] <= CLOSE_ENOUGH_Z
+    ) {
+      resetState(toggleFullGestureStart, "READY");
       var hand = frame.hands[0];
       var extendedFingers = 0;
-      for (var f = 0; f < hand.fingers.length; f++){
-          var finger = hand.fingers[f];
-          if(finger.extended) extendedFingers++;
+      for (var f = 0; f < hand.fingers.length; f++) {
+        var finger = hand.fingers[f];
+        if (finger.extended) extendedFingers++;
       }
 
       // seek gesture start
       if (hand.indexFinger.extended && extendedFingers == 1) {
-        resetState(volumeGestureStart, 'READY');
-        resetState(togglePlayGestureStart, 'READY');
-        if (seekGestureStart.state == 'READY') {
+        resetState(volumeGestureStart, "READY");
+        resetState(togglePlayGestureStart, "READY");
+        if (seekGestureStart.state == "READY") {
           feedbackString = "seeking";
 
-          resetState(seekGestureStart, 'STARTED');
+          resetState(seekGestureStart, "STARTED");
           seekGestureStart.init_pos = hand.palmPosition[0]; // save y position
           seekGestureStart.init_time = player.getPosition();
         }
@@ -197,72 +237,86 @@ window.onload = () => {
 
       // volume gesture start
       else if (hand.pinchStrength >= GRAB_THRESH) {
-        resetState(seekGestureStart, 'READY');
-        resetState(togglePlayGestureStart, 'READY');
+        resetState(seekGestureStart, "READY");
+        resetState(togglePlayGestureStart, "READY");
 
-        if (volumeGestureStart.state == 'READY') {
+        if (volumeGestureStart.state == "READY") {
           feedbackString += "adjusting volume";
-          resetState(volumeGestureStart, 'STARTED');
+          resetState(volumeGestureStart, "STARTED");
           volumeGestureStart.init_pos = hand.palmPosition[1]; // save y position
           volumeGestureStart.init_vol = player.getVolume();
-
         }
 
-      // play/pause gesture start
+        // play/pause gesture start
       } else {
-        resetState(seekGestureStart, 'READY');
-        resetState(volumeGestureStart, 'READY');
-        if (togglePlayGestureStart.state == 'READY') {
-
+        resetState(seekGestureStart, "READY");
+        resetState(volumeGestureStart, "READY");
+        if (togglePlayGestureStart.state == "READY") {
           // if palm is flat-ish, and pause/play toggle is ready
-          resetState(togglePlayGestureStart, 'STARTED');
+          resetState(togglePlayGestureStart, "STARTED");
           togglePlayGestureStart.init_pos = hand.palmPosition[1];
         }
       }
 
-
       /////// ----------------------------- ////////
       // toggle play logic
 
-      if (togglePlayGestureStart.state == 'STARTED') {
-        resetState(seekGestureStart, 'WAITING'); // to avoid transitioning to seeking
+      if (togglePlayGestureStart.state == "STARTED") {
+        resetState(seekGestureStart, "WAITING"); // to avoid transitioning to seeking
         feedbackString = "move hand down to toggle play/pause";
         // detect for y-axis speed
-        if (hand.palmVelocity[1] <= -VEL_THRESH && hand.palmPosition[1] - togglePlayGestureStart.init_pos <= -Y_CHANGE) {
+        if (
+          hand.palmVelocity[1] <= -VEL_THRESH &&
+          hand.palmPosition[1] - togglePlayGestureStart.init_pos <= -Y_CHANGE
+        ) {
           // toggles
-          player.play()
+          player.play();
           feedbackString = player.getState();
-          resetState(togglePlayGestureStart, 'WAITING');
+          resetState(togglePlayGestureStart, "WAITING");
         }
       }
       /////// ----------------------------- ////////
       // adjust volume and seek logic
-      if (volumeGestureStart.state == 'STARTED') {
-        resetState(seekGestureStart, 'WAITING'); // to avoid transitioning to seeking
-        var volChange = VOL_RATIO*(hand.palmPosition[1] - volumeGestureStart.init_pos);
-        player.setVolume(round5(Math.max(0, Math.min(100, volumeGestureStart.init_vol + volChange))));
-        feedbackString = 'setting volume: ' + round5(Math.max(0, Math.min(100, volumeGestureStart.init_vol + volChange))).toString();
-
+      if (volumeGestureStart.state == "STARTED") {
+        resetState(seekGestureStart, "WAITING"); // to avoid transitioning to seeking
+        var volChange =
+          VOL_RATIO * (hand.palmPosition[1] - volumeGestureStart.init_pos);
+        player.setVolume(
+          round5(
+            Math.max(0, Math.min(100, volumeGestureStart.init_vol + volChange))
+          )
+        );
+        feedbackString =
+          "setting volume: " +
+          round5(
+            Math.max(0, Math.min(100, volumeGestureStart.init_vol + volChange))
+          ).toString();
       }
-      if (seekGestureStart.state == 'STARTED') {
-        resetState(volumeGestureStart, 'WAITING');
-        var seekChange = SEEK_RATIO*(hand.palmPosition[0] + X_AXIS_SHIFT);
+      if (seekGestureStart.state == "STARTED") {
+        resetState(volumeGestureStart, "WAITING");
+        var seekChange = SEEK_RATIO * (hand.palmPosition[0] + X_AXIS_SHIFT);
         var seekTime = Math.min(Math.max(0, seekChange), player.getDuration());
         player.seek(seekTime);
-        var seconds = (Math.floor(seekTime % 60)).toString();
+        var seconds = Math.floor(seekTime % 60).toString();
         if (seconds.length < 2) {
-          seconds = seconds+'0';
+          seconds = seconds + "0";
         }
-        feedbackString = 'seeking to time: ' + (Math.floor(seekTime/60)).toString() +":"+ seconds;
+        feedbackString =
+          "seeking to time: " +
+          Math.floor(seekTime / 60).toString() +
+          ":" +
+          seconds;
       }
-
-    }
-
-    else if (frame.hands.length == 2 && frame.hands[0].palmPosition[2] <= CLOSE_ENOUGH_Z && frame.hands[1].palmPosition[2] <= CLOSE_ENOUGH_Z) { // only detect for one person
+    } else if (
+      frame.hands.length == 2 &&
+      frame.hands[0].palmPosition[2] <= CLOSE_ENOUGH_Z &&
+      frame.hands[1].palmPosition[2] <= CLOSE_ENOUGH_Z
+    ) {
+      // only detect for one person
       hands = frame.hands;
-      resetState(togglePlayGestureStart, 'READY');
-      resetState(volumeGestureStart, 'READY');
-      resetState(seekGestureStart, 'READY');
+      resetState(togglePlayGestureStart, "READY");
+      resetState(volumeGestureStart, "READY");
+      resetState(seekGestureStart, "READY");
 
       if (hands[0].palmPosition[0] < hands[1].palmPosition[0]) {
         leftHand = hands[0];
@@ -273,19 +327,17 @@ window.onload = () => {
       }
 
       // detect if hands are facing each other palm side
-      if (toggleFullGestureStart.state == 'READY') {
-
+      if (toggleFullGestureStart.state == "READY") {
         // if ready to change screens, switch to STARTED state
-        resetState(toggleFullGestureStart, 'STARTED');
+        resetState(toggleFullGestureStart, "STARTED");
 
         // detect left and right hands
-
 
         toggleFullGestureStart.init_left = leftHand.palmPosition[0];
         toggleFullGestureStart.init_right = rightHand.palmPosition[0];
       }
       // if enough frames have passed or
-      if (toggleFullGestureStart.state == 'STARTED') {
+      if (toggleFullGestureStart.state == "STARTED") {
         feedbackString = "move out to full screen, move in to small screen";
 
         // if left and right hands are moving fast enough in certain diretions
@@ -293,58 +345,81 @@ window.onload = () => {
         if (
           leftHand.palmVelocity[0] < VEL_THRESH_FULL &&
           rightHand.palmVelocity[0] > VEL_THRESH_FULL &&
-          leftHand.palmPosition[0] - toggleFullGestureStart.init_left <= -X_CHANGE &&
-          rightHand.palmPosition[0] - toggleFullGestureStart.init_right >= X_CHANGE
+          leftHand.palmPosition[0] - toggleFullGestureStart.init_left <=
+            -X_CHANGE &&
+          rightHand.palmPosition[0] - toggleFullGestureStart.init_right >=
+            X_CHANGE
         ) {
           if (player.getWidth() == INIT_WIDTH) {
-            player.resize(window.innerWidth-150, ASPECT_RATIO*(window.innerWidth-150));
-            document.getElementById('feedback').style.width = window.innerWidth - 150 - 10;
-            resetState(toggleFullGestureStart, 'WAITING');
+            player.resize(
+              window.innerWidth - 150,
+              ASPECT_RATIO * (window.innerWidth - 150)
+            );
+            document.getElementById("feedback").style.width =
+              window.innerWidth - 150 - 10;
+            resetState(toggleFullGestureStart, "WAITING");
           }
         } else if (
           leftHand.palmVelocity[0] > VEL_THRESH_FULL &&
           rightHand.palmVelocity[0] < VEL_THRESH_FULL &&
-          leftHand.palmPosition[0] - toggleFullGestureStart.init_left >= X_CHANGE &&
-          rightHand.palmPosition[0] - toggleFullGestureStart.init_right <= -X_CHANGE
+          leftHand.palmPosition[0] - toggleFullGestureStart.init_left >=
+            X_CHANGE &&
+          rightHand.palmPosition[0] - toggleFullGestureStart.init_right <=
+            -X_CHANGE
         ) {
           if (player.getWidth() != INIT_WIDTH) {
-            player.resize(INIT_WIDTH, ASPECT_RATIO*INIT_WIDTH);
-            document.getElementById('feedback').style.width = INIT_WIDTH - 10;
-            resetState(toggleFullGestureStart, 'WAITING');
+            player.resize(INIT_WIDTH, ASPECT_RATIO * INIT_WIDTH);
+            document.getElementById("feedback").style.width = INIT_WIDTH - 10;
+            resetState(toggleFullGestureStart, "WAITING");
           }
         }
-
       }
-
     }
-    ///////////////////////
-    // debugging
+
     var handString = "";
-    var frameString = "Frame ID: " + frame.id  + "<br />"
-                  + "Timestamp: " + frame.timestamp + " &micro;s<br />"
-                  + "Hands: " + frame.hands.length + "<br />"
-                  + "Fingers: " + frame.fingers.length + "<br />"
-                  + "Tools: " + frame.tools.length + "<br />"
-                  + "Gestures: " + frame.gestures.length + "<br />";
+    var frameString =
+      "Frame ID: " +
+      frame.id +
+      "<br />" +
+      "Timestamp: " +
+      frame.timestamp +
+      " &micro;s<br />" +
+      "Hands: " +
+      frame.hands.length +
+      "<br />" +
+      "Fingers: " +
+      frame.fingers.length +
+      "<br />" +
+      "Tools: " +
+      frame.tools.length +
+      "<br />" +
+      "Gestures: " +
+      frame.gestures.length +
+      "<br />";
 
     if (frame.hands.length > 0) {
       for (var i = 0; i < frame.hands.length; i++) {
-
         var hand = frame.hands[i];
 
         handString += "Hand ID: " + hand.id + "<br />";
-        handString += "Direction: " + vectorToString(hand.direction, 2) + "<br />";
-        handString += "Palm normal: " + vectorToString(hand.palmNormal, 2) + "<br />";
-        handString += "Palm position: " + vectorToString(hand.palmPosition) + " mm<br />";
-        handString += "Palm velocity: " + vectorToString(hand.palmVelocity) + " mm/s<br />";
-        handString += "Sphere center: " + vectorToString(hand.sphereCenter) + " mm<br />";
-        handString += "Sphere radius: " + hand.sphereRadius.toFixed(1) + " mm<br />";
+        handString +=
+          "Direction: " + vectorToString(hand.direction, 2) + "<br />";
+        handString +=
+          "Palm normal: " + vectorToString(hand.palmNormal, 2) + "<br />";
+        handString +=
+          "Palm position: " + vectorToString(hand.palmPosition) + " mm<br />";
+        handString +=
+          "Palm velocity: " + vectorToString(hand.palmVelocity) + " mm/s<br />";
+        handString +=
+          "Sphere center: " + vectorToString(hand.sphereCenter) + " mm<br />";
+        handString +=
+          "Sphere radius: " + hand.sphereRadius.toFixed(1) + " mm<br />";
 
         // And so on...
       }
     }
     // uncomment for debug
     // document.getElementById('frame_info').innerHTML = frameString + handString;
-    document.getElementById('feedback').innerHTML = feedbackString;
+    document.getElementById("feedback").innerHTML = feedbackString;
   });
-}
+};
